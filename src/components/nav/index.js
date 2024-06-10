9d5871fc78ca68c9d29c5857053de3282eeb9025
oier <oieruni22@gmail.com>
2024-06-10 19:08:30 +0200

    Add Nav
    Adding Nav component.

---
+++ src/components/nav/index.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import './nav.css';
+
+export const Nav = ({ state, send }) => {
+
+  const goToInitial = () => {
+    send({type: "CANCEL"})
+  }
+
+  return (
+    <nav className='Nav'>
+      <h1 className='Nav-logo'>Book a fly ✈</h1>
+      {!state.matches('initial') &&
+        <button
+          className='Nav-cancel button-secondary'
+          onClick={goToInitial}
+        >
+          Cancelar
+        </button>
+      }
+      
+    </nav>
+  );
+}; 
