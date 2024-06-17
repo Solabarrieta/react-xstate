3d7cc24b6123758328a2a1a6d8aeeea474362fae
oier <oieruni22@gmail.com>
2024-06-17 12:55:46 +0200

    Add context
    Adding context to Tickets to get Passengers names and Selected Country.

--- src/components/tickets/index.js
+++ src/components/tickets/index.js
@@ -10,9 +10,10 @@ export const Tickets = ({ send, context }) => {
     <div className='Tickets'>
       <p className='Tickets-description description'>Gracias por volar con book a fly 💚</p>
       <div className='Tickets-ticket'>
-        <div className='Tickets-country'>Colombia</div>
+        <div className='Tickets-country'>{context.selectedCountry}</div>
         <div className='Tickets-passengers'>
           <span>✈</span>
+          {context.passengers.map((passenger, idx) => <p key={idx}>{passenger}</p>)}
         </div>
       </div>
       <button
