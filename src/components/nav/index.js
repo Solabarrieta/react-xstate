720ca26cbad8968f984ad57eb5f7adf1177e1b83
oier <oieruni22@gmail.com>
2024-06-17 12:51:49 +0200

    Refactor
    Removing cancel button from nav at the last state.

--- src/components/nav/index.js
+++ src/components/nav/index.js
@@ -10,7 +10,7 @@ export const Nav = ({ state, send }) => {
   return (
     <nav className='Nav'>
       <h1 className='Nav-logo'>Book a fly ✈</h1>
-      {!state.matches('initial') &&
+      {!state.matches('initial') && !state.matches('tickets') &&
         <button
           className='Nav-cancel button-secondary'
           onClick={goToInitial}
